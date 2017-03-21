2b850dfe2c7a7897fd6db83ec421ec3182b593e7
Muralidharan <kmuralidharan@GMX0007MC.local>
2017-03-21 17:13:47 +0530

    tweaked few routing logics to learn about framework

--- handler.js
+++ handler.js
@@ -1,11 +1,22 @@
 'use strict';
 
+// invoked when hello route is hit
 module.exports.hello = (event, context, callback) => {
+
+  if(event) {
+    var routeUrl = 'https://'+event.headers.Host+'/'+event.requestContext.stage+'/index';
+  }
+  else{
+    var routeUrl = 'https://1f1hrv88cd.execute-api.us-east-1.amazonaws.com/dev/index'
+  }
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      url: routeUrl,
+      input: event, // to display event object
+      context: context // to get info about context object
     }),
   };
 
--- optimus.js
+++ optimus.js
@@ -4,41 +4,63 @@ const request=require('request');
 
 // called when index route is hit
 module.exports.index = (event, context, callback) => {
+  // to test the get call in nodejs
+  request.get('https://www-dev.uat-thesun.co.uk/wp-json/wp/v2/posts',null,function(err,res,body){
+      
+      if(err) 
+        //to handle when there is an error
+        callback(null, err)
 
-// to test the get call in nodejs
-request.get('https://www-dev.uat-thesun.co.uk/wp-json/wp/v2/posts',null,function(err,res,body){
-    if(err) //TODO: handle err
-    if(res.statusCode !== 200 ) //etc
-    //TODO Do something with response
-    var response = JSON.parse(res.body); 
+      if(event) {
+        var routeUrl = 'https://'+event.headers.Host+'/'+event.requestContext.stage+'/hello';
+      }
+      else{
+        var routeUrl = 'https://1f1hrv88cd.execute-api.us-east-1.amazonaws.com/dev/hello'
+      }
+
+      if(res.statusCode !== 200 )
+        // To parse data when rest call succeeds 
+        var data = JSON.parse(res.body);
+        var rendered = {
+          statusCode: 200,
+          body: JSON.stringify({
+            code: data.code,
+            message: data.message,
+            data: data.data,
+            url: routeUrl,
+          }),
+        };
+        callback(null, rendered)
   });
-
-   const rendered = {
-    statusCode: 200,
-    body: JSON.stringify({
-      code: response.code,
-      msg: response.message,
-      data: response.data
-    }),
-  };
-
-  callback(null, rendered)
 };
 
 // called internally when test route is hit
-let hello = () => {
-  return 'Hello world';
+let hello = (event) => {
+  
+  // when name is passed as query param
+  if (event.pathParameters && event.pathParameters.name)
+    return 'Hello! '+ event.pathParameters.name;
+  else
+    return 'Hello! world';
 };
 
 // called when test route is hit
 module.exports.test = (event, context, callback) => {
 
+    if(event) {
+        var routeUrl = 'https://'+event.headers.Host+'/'+event.requestContext.stage+'/hello';
+    }
+    else{
+      var routeUrl = 'https://1f1hrv88cd.execute-api.us-east-1.amazonaws.com/dev/hello'
+    }
+
    const response = {
     statusCode: 200,
     body: JSON.stringify({
-      test: hello(),
+      test: hello(event),
       Status: 'ok',
-      match: 'IndVsAus'
+      match: 'IndVsAus',
+      url: routeUrl,
     }),
   };
 
